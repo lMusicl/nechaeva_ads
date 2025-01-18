f06a0a66934b7d57f53803dd72379f55728edfc7
lMusicl <dmezhenskij@yandex.ru>
2025-01-18 17:11:44 +0300

    Animated

--- src/scripts.js
+++ src/scripts.js
@@ -1,19 +1,26 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
+    new WOW().init();
+
     const swiper = new Swiper('.case-slider', {
         spaceBetween: 30,
         slidesPerView: 2,
+        speed: 2000,
+        autoplay: {
+            delay: 1000,
+            disableOnInteraction: false,
+        },
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
     });
 
     // Лайтбокс функционал
     const lightbox = $('.lightbox');
     const lightboxImg = $('.lightbox img');
-    
+
     // Открытие лайтбокса при клике на слайд
-    $('.swiper-slide').click(function() {
+    $('.swiper-slide').click(function () {
         const bgImage = $(this).find('.swiper-slide-image').css('background-image');
         const imageUrl = bgImage.replace(/url\(['"](.+)['"]\)/, '$1');
         lightboxImg.attr('src', imageUrl);
@@ -21,21 +28,21 @@ jQuery(document).ready(function($) {
     });
 
     // Закрытие лайтбокса
-    lightbox.click(function(e) {
+    lightbox.click(function (e) {
         if ($(e.target).hasClass('lightbox') || $(e.target).hasClass('lightbox-close')) {
             lightbox.fadeOut(300);
         }
     });
 
     // Закрытие по Escape
-    $(document).keydown(function(e) {
+    $(document).keydown(function (e) {
         if (e.key === "Escape") {
             lightbox.fadeOut(300);
         }
     });
 
     // Плавный скролл к якорю
-    $('a[href^="#"]').on('click', function(e) {
+    $('a[href^="#"]').on('click', function (e) {
         e.preventDefault();
         const target = $(this.hash);
         if (target.length) {
@@ -46,20 +53,20 @@ jQuery(document).ready(function($) {
     });
 
     // Обработка отправки формы
-    $('.form-content-left-form').on('submit', function(e) {
+    $('.form-content-left-form').on('submit', function (e) {
         e.preventDefault();
-        
+
         $.ajax({
             type: 'POST',
             url: 'send.php',
             data: $(this).serialize(),
             dataType: 'json',
-            success: function(response) {
-                if(response.success) {
+            success: function (response) {
+                if (response.success) {
                     $('.form-content-left-form')[0].reset();
                 }
             },
-            error: function() {
+            error: function () {
                 console.log('Ошибка отправки формы');
             }
         });
